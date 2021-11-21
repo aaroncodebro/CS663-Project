9f62ae600d0d5014f64aa93b2055a4dfd06b15e0
Tejas <tejaspagare2002@gmail.com>
2021-11-22 03:07:51 +0530

    remove object

---
+++ removeObject.m
@@ -0,0 +1,22 @@
+clc; clear;
+img = imread('gp.jpg');
+img = double(img);
+figure, imshow(img/255);
+
+rem = images.roi.AssistedFreehand('Closed',true,'Color','r');
+maintain = images.roi.AssistedFreehand('Closed',true,'Color','b');
+draw(rem);draw(maintain);
+
+mask_remove = createMask(rem);
+mask_maintain = createMask(maintain);
+
+% if they overlap, then maintain overlapped version
+mask_remove = (mask_remove - mask_maintain)>0;
+mask = mask_maintain-mask_remove;
+% How much to remove
+rowDiff = 0       ; colDiff = 10;
+
+ForwardCarve = SeamCarveDP([rows-rowDiff, cols-colDiff], img, mask, 'FE');
+% BackwardCarve = SeamCarveDP([rows-rowDiff, cols-colDiff], img, mask, 'BE');
+figure, imshow(ForwardCarve/255);
+% figure, imshow(BackwardCarve/255);
